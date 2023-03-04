7d9d963a871f04c577b19bce6ee278926951a2e6
satishkumar <satishsajjan4564@gmail.com>
2023-03-04 10:30:34 +0530

    Added Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Navbar,
+  Hero,
+  Feedbacks,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className='relative  z-0'>
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [Active, setActive] = useState('');
+  const [Toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py- fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer '>
+            Satishkumar
+          </p>
+        </Link>
+        <ul
+          className='list-none hidden sm:flex flex-row
+         gap-10'
+        >
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                Active === link.title ? 'text-white' : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={Toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer '
+            onClick={() => setToggle(!Toggle)}
+          />
+          <div
+            className={`${
+              !Toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    Active === link.title ? 'text-white' : 'text-secondary'
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!Toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
