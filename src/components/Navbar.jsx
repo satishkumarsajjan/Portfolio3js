ded411a96766b1ff5cb383ff84b08c37c697de52
satishkumar <satishsajjan4564@gmail.com>
2023-03-04 13:12:32 +0530

    added 3d model to Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [Toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py- fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-transparent`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
